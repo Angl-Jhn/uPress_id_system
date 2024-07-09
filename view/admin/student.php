0947199d473dc939a3384d23b93eb1c0081a807a
Khalil-Est <qb202100149@wmsu.edu.ph>
2024-07-10 02:00:54 +0800

    ID LAYOUT (STUDENT)
    A reconstruction of the ID layout for students

--- view/admin/student.php
+++ view/admin/student.php
@@ -1,7 +1,15 @@
             <main class="content px-3 py-2">
-                <div class="container-fluid">
-                    dasd
-                    <!-- Elements -->
-                    
+                <div style="display: block; margin: auto;">
+                    <img src="../../assets/id_layouts/STUDENT-ID-FRONT.svg" alt="STUDENT ID LAYOUT" style="max-width:204px; max-height:321px; display: block; margin: auto;">
+                    <img src="../../assets/id_layouts/signature.png" style="position: relative; z-index: 2; transform: translate(0%, -110%); max-width:100px; max-height:50px; display: block; margin: auto;">
+                    <p style="color: black; text-align: center; position: relative; z-index: 1; transform: translate(0%, -650%); font-weight: bold; font-size: 8px">PM Winston Churchill</p>
+                    <p style="color: black; text-align: center; position: realtive; z-index: 1; transform: translate(0%, -800%); font-size: 8px">British Prime Minister</p>
+                </div>
+                <div style="display: block; margin: auto;">
+                    <img src="../../assets/id_layouts/STUDENT-ID-BACK.svg" alt="STUDENT ID LAYOUT" style="max-width:204px; max-height:321px; display: block; margin: auto;">
+                    <p style="color: black; text-align: center; position: relative; z-index: 1; font-size: 5px; font-weight: bold; transform: translate(-6.4%, -1850%)">2024-2025</p>
+                    <p style="color: black; text-align: center; position: relative; z-index: 1; font-size: 5px; font-weight: bold; transform: translate(-6.4%, -1930%)">2025-2026</p>
+                    <p style="color: black; text-align: center; position: relative; z-index: 1; font-size: 5px; font-weight: bold; transform: translate(-6.4%, -2020%)">2026-2027</p>
+                    <p style="color: black; text-align: center; position: relative; z-index: 1; font-size: 5px; font-weight: bold; transform: translate(-6.4%, -2110%)">2027-2028</p>
                 </div>
             </main>
