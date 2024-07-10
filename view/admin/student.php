8ff77f7e92279967cc8d04f9f43f688e6b714cc3
Khalil-Est <qb202100149@wmsu.edu.ph>
2024-07-10 10:28:13 +0800

    Update student.php

--- view/admin/student.php
+++ view/admin/student.php
@@ -4,6 +4,9 @@
                     <img src="../../assets/id_layouts/signature.png" style="position: relative; z-index: 2; transform: translate(0%, -110%); max-width:100px; max-height:50px; display: block; margin: auto;">
                     <p style="color: black; text-align: center; position: relative; z-index: 1; transform: translate(0%, -650%); font-weight: bold; font-size: 8px">PM Winston Churchill</p>
                     <p style="color: black; text-align: center; position: realtive; z-index: 1; transform: translate(0%, -800%); font-size: 8px">British Prime Minister</p>
+                    <p style="color: black; text-align: center; position: realtive; z-index: 1; transform: translate(-2.8%, -1350%); font-weight: bold; font-size: 10px">2021-00149</p>
+                    <p style="color: black; text-align: center; position: realtive; z-index: 1; transform: translate(0%, -900%); font-weight: 900; font-size: 16px; color: maroon">BS CS</p>
+                    <p style="color: black; text-align: center; position: realtive; z-index: 1; transform: translate(0%, -1000%); font-weight: bold; font-size: 16px;">SHAN KHYLE D. ESTRADA</p>
                 </div>
                 <div style="display: block; margin: auto;">
                     <img src="../../assets/id_layouts/STUDENT-ID-BACK.svg" alt="STUDENT ID LAYOUT" style="max-width:204px; max-height:321px; display: block; margin: auto;">
