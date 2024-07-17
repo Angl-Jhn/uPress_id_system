b7a4a172d5f84ba236d2f519aedb73b93f24f8be
Khalil-Est <qb202100149@wmsu.edu.ph>
2024-07-18 00:21:24 +0800

    BETTER ID LAYOUT (STUDENT)
    Revising the code for students' id layout so that it remains the same even when the size of the viewport is adjusted for different screen sizes.

--- view/admin/student.php
+++ view/admin/student.php
@@ -1,55 +1,51 @@
             <main class="content px-3 py-2">
                 <div class="row">
                     <div class="col-md-6">
-                        <div style="display: block; margin: auto;">
-                            <img src="../../assets/id_layouts/STUDENT-ID-FRONT.svg" alt="STUDENT ID LAYOUT"
-                                style="max-width:204px; max-height:321px; display: block; margin: auto;">
-                            <img src="../../assets/id_layouts/signature.png"
-                                style="position: relative; z-index: 2; transform: translate(0%, -110%); max-width:100px; max-height:50px; display: block; margin: auto;">
+                        <div style="position: relative; display: block; background-image: url('../../assets/id_layouts/STUDENT/STUDENT-ID-FRONT.svg'); width:204px; height:316px; background-size: 204px 321px; background-position: center; background-repeat: no-repeat; border: 1px solid black; border-radius: 10px">
                             <p
-                                style="color: black; text-align: center; position: relative; z-index: 1; transform: translate(0%, -650%); font-weight: bold; font-size: 8px">
-                                PM Winston Churchill</p>
-                            <p
-                                style="color: black; text-align: center; position: realtive; z-index: 1; transform: translate(0%, -800%); font-size: 8px">
-                                British Prime Minister</p>
-                            <p
-                                style="color: black; text-align: center; position: realtive; z-index: 1; transform: translate(-2.8%, -1350%); font-weight: bold; font-size: 10px">
+                                style="position: absolute; left: 45px; top: 215px; color: black; text-align: center; z-index: 1; font-weight: bold; font-size: 10px">
                                 2021-00149</p>
                             <p
-                                style="color: black; text-align: center; position: realtive; z-index: 1; transform: translate(0%, -900%); font-weight: 900; font-size: 16px; color: maroon">
+                                style="position: absolute; left: 0; right: 0; top: 230px; color: black; text-align: center; z-index: 1; font-weight: 900; font-size: 16px; color: maroon">
                                 BS CS</p>
                             <p
-                                style="color: black; text-align: center; position: realtive; z-index: 1; transform: translate(0%, -1000%); font-weight: bold; font-size: 16px;">
+                                style="position: absolute; left: 0; right: 0; top: 250px; color: black; text-align: center; z-index: 1; font-weight: bold; font-size: 16px;">
                                 SHAN KHYLE D. ESTRADA</p>
+                            <img src="../../assets/id_layouts/signature.png"
+                                style="position: absolute; left: 0; right: 0; top: 270px; max-width:60px; max-height:30px; display: block; margin: auto;">
+                            <p
+                                style="position: absolute; left: 0; right: 0; bottom: 1px; color: black; text-align: center; z-index: 1; font-weight: bold; font-size: 8px">
+                                PM Winston Churchill</p>
+                            <p
+                                style="position: absolute; left: 0; right: 0; bottom: 10px; color: black; text-align: center; z-index: 1; font-size: 8px; padding-bottom: 0px; margin: 0px;">
+                                British Prime Minister</p>
                         </div>
                     </div>
                     <div class="col-md-6">
-                        <div style="display: block; margin: auto;">
-                            <img src="../../assets/id_layouts/STUDENT-ID-BACK.svg" alt="STUDENT ID LAYOUT"
-                                style="max-width:204px; max-height:321px; display: block; margin: auto;">
+                        <div style="position: relative; display: block; background-image: url('../../assets/id_layouts/STUDENT/STUDENT-ID-BACK.svg'); width:204px; height:316px; background-size: 204px 321px; background-position: center; background-repeat: no-repeat; border: 1px solid black; border-radius: 10px">
                             <p
-                                style="color: black; text-align: center; position: relative; z-index: 1; font-size: 5px; font-weight: bold; transform: translate(-6.4%, -1850%)">
-                                2024-2025</p>
-                            <p
-                                style="color: black; text-align: center; position: relative; z-index: 1; font-size: 5px; font-weight: bold; transform: translate(-6.4%, -1930%)">
-                                2025-2026</p>
-                            <p
-                                style="color: black; text-align: center; position: relative; z-index: 1; font-size: 5px; font-weight: bold; transform: translate(-6.4%, -2020%)">
-                                2026-2027</p>
-                            <p
-                                style="color: black; text-align: center; position: relative; z-index: 1; font-size: 5px; font-weight: bold; transform: translate(-6.4%, -2110%)">
-                                2027-2028</p>
-                            <p
-                                style="color: black; text-align: center; position: relative; z-index: 1; font-size: 14px; font-weight: 900; transform: translate(0%, -1600%)">
+                                style="position: absolute; left: 0; right: 0; top: 73px; color: black; text-align: center; font-size: 14px; font-weight: 900;">
                                 GILDA D. ESTRADA</p>
                             <p
-                                style="color: black; text-align: center; position: relative; z-index: 1; font-size: 10px; transform: translate(0%, -2330%)">
+                                style="position: absolute; left: 0; right: 0; top: 100px; color: black; text-align: center; font-size: 10px;">
                                 San Roque Gil S. Estrada Compound Z.C.</p>
                             <p
-                                style="color: black; text-align: center; position: relative; z-index: 1; font-size: 12px; font-weight: 900; transform: translate(0%, -2000%)">
+                                style="position: absolute; left: 0; right: 0; top: 120px; color: black; text-align: center; font-size: 12px; font-weight: 900;">
                                 0906-164-8925</p>
+                            <p
+                                style="position: absolute; left: 11px; bottom: 115px; color: black; font-size: 5px; font-weight: bold;">
+                                2024-2025</p>
+                            <p
+                                style="position: absolute; left: 11px; bottom: 98px; color: black; font-size: 5px; font-weight: bold;">
+                                2025-2026</p>
+                            <p
+                                style="position: absolute; left: 11px; bottom: 80px; color: black; font-size: 5px; font-weight: bold;">
+                                2026-2027</p>
+                            <p
+                                style="position: absolute; left: 11px; bottom: 63px; color: black; font-size: 5px; font-weight: bold;">
+                                2027-2028</p>
                             <img src="../../assets/id_layouts/signature.png"
-                                style="position: relative; z-index: 2; transform: translate(0%, -530%); max-width:100px; max-height:50px; display: block; margin: auto;">
+                                style="position: absolute; left: 0; right: 0; bottom: 25px; max-width:80px; max-height:40px; display: block; margin: auto;">
                         </div>
                     </div>
                 </div>
