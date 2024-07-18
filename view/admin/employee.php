be53ab96a10e62568f0b71aaca4fc814b9ff9cc8
Khalil-Est <qb202100149@wmsu.edu.ph>
2024-07-18 20:13:58 +0800

    ID LAYOUT RESOURCES

--- view/admin/employee.php
+++ view/admin/employee.php
@@ -1,7 +1,27 @@
             <main class="content px-3 py-2">
-                <div class="container-fluid">
-                    dasd
-                    <!-- Elements -->
-                    
+            <div class="row">
+                    <div class="col-md-6">
+                        <div style="position: relative; display: block; background-image: url('../../assets/id_layouts/EMPLOYEE/CSM.png'); width:316px; height: 204px; background-size: 321px 204px; background-position: center; background-repeat: no-repeat; border: 1px solid black; border-radius: 10px">
+                                <p
+                                    style="position: absolute; left: 45px; top: 215px; color: black; text-align: center; z-index: 1; font-weight: bold; font-size: 10px">
+                                    2021-00149</p>
+                                <p
+                                    style="position: absolute; left: 0; right: 0; top: 230px; color: black; text-align: center; z-index: 1; font-weight: 900; font-size: 16px; color: maroon">
+                                    BS CS</p>
+                                <p
+                                    style="position: absolute; left: 0; right: 0; top: 250px; color: black; text-align: center; z-index: 1; font-weight: bold; font-size: 16px;">
+                                    SHAN KHYLE D. ESTRADA</p>
+                                <img src="../../assets/id_layouts/signature.png"
+                                    style="position: absolute; left: 0; right: 0; top: 270px; max-width:60px; max-height:30px; display: block; margin: auto;">
+                                <p
+                                    style="position: absolute; left: 0; right: 0; bottom: 1px; color: black; text-align: center; z-index: 1; font-weight: bold; font-size: 8px">
+                                    PM Winston Churchill</p>
+                                <p
+                                    style="position: absolute; left: 0; right: 0; bottom: 10px; color: black; text-align: center; z-index: 1; font-size: 8px; padding-bottom: 0px; margin: 0px;">
+                                    British Prime Minister</p>
+                            </div>
+                    </div>
+                    <div class="col-md-6">
+                    </div>
                 </div>
             </main>
