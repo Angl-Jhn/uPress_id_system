c44f74e4892a2053c3dce5171a4177f25d5e20ea
Khalil-Est <qb202100149@wmsu.edu.ph>
2024-07-24 07:53:53 +0800

    INDEX ID LAYOUT CHANGED

--- view/shared/body-content.php
+++ view/shared/body-content.php
@@ -36,13 +36,53 @@ $page_title="titi";
                     <h2>Student ID</h2>
                     <div class="row justify-content-center py-5">
                         <div class="col-lg-4">
-                            <img src="../../assets/custom/id-layout/front-student.svg" class="img-fluid"
-                                alt="Student ID Front">
+                            <div style="position: relative; left: 50%; transform: translateX(-50%); display: block; background-image: url('../../assets/id_layouts/STUDENT/STUDENT-ID-FRONT.svg'); width:204px; height:316px; background-size: 204px 321px; background-position: center; background-repeat: no-repeat; border: 1px solid black; border-radius: 10px">
+                                <p
+                                    style="position: absolute; left: 45px; top: 215px; color: black; text-align: center; z-index: 1; font-weight: bold; font-size: 10px">
+                                    2021-00149</p>
+                                <p
+                                    style="position: absolute; left: 0; right: 0; top: 230px; color: black; text-align: center; z-index: 1; font-weight: 900; font-size: 16px; color: maroon">
+                                    BS CS</p>
+                                <p
+                                    style="position: absolute; left: 0; right: 0; top: 250px; color: black; text-align: center; z-index: 1; font-weight: bold; font-size: 16px;">
+                                    SHAN KHYLE D. ESTRADA</p>
+                                <img src="../../assets/id_layouts/signature.png"
+                                    style="position: absolute; left: 0; right: 0; top: 270px; max-width:60px; max-height:30px; display: block; margin: auto;">
+                                <p
+                                    style="position: absolute; left: 0; right: 0; bottom: 1px; color: black; text-align: center; z-index: 1; font-weight: bold; font-size: 8px">
+                                    PM Winston Churchill</p>
+                                <p
+                                    style="position: absolute; left: 0; right: 0; bottom: 10px; color: black; text-align: center; z-index: 1; font-size: 8px; padding-bottom: 0px; margin: 0px;">
+                                    British Prime Minister</p>
+                            </div>
                             <h3>Front</h3>
                         </div>
                         <div class="col-lg-4">
-                            <img src="../../assets/custom/id-layout/back-student.svg" class="img-fluid"
-                                alt="Student ID Back">
+                                <div style="position: relative; left: 50%; transform: translateX(-50%); display: block; background-image: url('../../assets/id_layouts/STUDENT/STUDENT-ID-BACK.svg'); width:204px; height:316px; background-size: 204px 321px; background-position: center; background-repeat: no-repeat; border: 1px solid black; border-radius: 10px">
+                                    <p
+                                        style="position: absolute; left: 0; right: 0; top: 73px; color: black; text-align: center; font-size: 14px; font-weight: 900;">
+                                        GILDA D. ESTRADA</p>
+                                    <p
+                                        style="position: absolute; left: 0; right: 0; top: 100px; color: black; text-align: center; font-size: 10px;">
+                                        San Roque Gil S. Estrada Compound Z.C.</p>
+                                    <p
+                                        style="position: absolute; left: 0; right: 0; top: 120px; color: black; text-align: center; font-size: 12px; font-weight: 900;">
+                                        0906-164-8925</p>
+                                    <p
+                                        style="position: absolute; left: 11px; bottom: 115px; color: black; font-size: 5px; font-weight: bold;">
+                                        2024-2025</p>
+                                    <p
+                                        style="position: absolute; left: 11px; bottom: 98px; color: black; font-size: 5px; font-weight: bold;">
+                                        2025-2026</p>
+                                    <p
+                                        style="position: absolute; left: 11px; bottom: 80px; color: black; font-size: 5px; font-weight: bold;">
+                                        2026-2027</p>
+                                    <p
+                                        style="position: absolute; left: 11px; bottom: 63px; color: black; font-size: 5px; font-weight: bold;">
+                                        2027-2028</p>
+                                    <img src="../../assets/id_layouts/signature.png"
+                                        style="position: absolute; left: 0; right: 0; bottom: 25px; max-width:80px; max-height:40px; display: block; margin: auto;">
+                                </div>
                             <h3>Back</h3>
                         </div>
                     </div>
@@ -53,13 +93,59 @@ $page_title="titi";
                     <h2>Employee ID</h2>
                     <div class="row justify-content-center py-5">
                         <div class="col-lg-6">
-                            <img src="../../assets/custom/id-layout/front-employee.svg" class="img-fluid"
-                                alt="Employee ID Front">
+                        <div style="position: relative; left: 50%; transform: translateX(-50%); display: block; background-image: url('../../assets/id_layouts/EMPLOYEE/CSM.png'); width:316px; height: 204px; background-size: 321px 204px; background-position: center; background-repeat: no-repeat; border: 1px solid black; border-radius: 10px">
+                                <p
+                                    style="position: absolute; left: 45px; bottom: -14px; color: black; text-align: center; z-index: 1; font-weight: bold; font-size: 9px">
+                                    00149</p>
+                                <p
+                                    style="position: absolute; left: 145px; right: 10px; top: 62px; color: black; text-align: center; z-index: 1; font-weight: 600; font-size: 12px;">
+                                    SHAN KHYLE D. ESTRADA</p>
+                                    <p
+                                    style="position: absolute; left: 145px; right: 10px; top: 80px; color: black; text-align: center; z-index: 1; font-weight: 600; font-size: 9px;">
+                                    University Professor</p>
+                                <p
+                                    style="position: absolute; left: 145px; right: 10px; top: 93px; color: black; text-align: center; z-index: 1; font-weight: 600; font-size: 9px;">
+                                    College of Computing Studies</p>
+                                <img src="../../assets/id_layouts/signature.png"
+                                    style="position: absolute; right: 60px; bottom: 10px; max-width:60px; max-height:30px; display: block; margin: auto;">
+                                <p
+                                    style="position: absolute; left: 158px; right: 20px; bottom: -6px; color: black; text-align: center; z-index: 1; font-weight: 800; font-size: 6px">
+                                    MA. CARLA A. OCHOTORENA, RN, Ph.D</p>
+                                <p
+                                    style="position: absolute; right: 75px; bottom: 4px; color: black; text-align: center; z-index: 1; font-size: 6px; font-weight: bold; padding-bottom: 0px; margin: 0px;">
+                                    President</p>
+                            </div>
                             <h3>Front</h3>
                         </div>
                         <div class="col-lg-6">
-                            <img src="../../assets/custom/id-layout/back-employee.svg" class="img-fluid"
-                                alt="Employee ID Back">
+                        <div style="position: relative; left: 50%; transform: translateX(-50%); display: block; background-image: url('../../assets/id_layouts/EMPLOYEE/EMPLOYEE-ID-BACK.png'); width:316px; height: 204px; background-size: 321px 204px; background-position: center; background-repeat: no-repeat; border: 1px solid black; border-radius: 10px">
+                                    <p
+                                        style="position: absolute; left: 0; right: 0; top: 84px; color: black; text-align: center; z-index: 1; font-weight: 900; font-size: 10px;">
+                                        San Roque, Gil S. Estrada Compound, Z.C.</p>
+                                    <p
+                                        style="position: absolute; left: 60px; bottom: 69.6px; color: black; font-size: 5px; font-weight: 900;">
+                                        03/27/2003</p>
+                                    <p
+                                        style="position: absolute; left: 57px; bottom: 53.5px; color: black; font-size: 5px; font-weight: 900;">
+                                        09451830519</p>
+                                        <p
+                                        style="position: absolute; left: 145px; bottom: 69.6px; color: black; font-size: 5px; font-weight: 900;">
+                                        O+</p>
+                                    <p
+                                        style="position: absolute; left: 145px; bottom: 53.5px; color: black; font-size: 5px; font-weight: 900;">
+                                        S</p>
+                                    <p
+                                        style="position: absolute; left: 205px; bottom: 45px; color: black; font-size: 8px; font-weight: 900;">
+                                        Gilda D. Estrada</p>
+                                    <p
+                                        style="position: absolute; left: 175px; bottom: 30px; color: black; font-size: 6px; font-weight: 900;">
+                                        San Roque, Gil S. Estrada Compound, Z.C.</p>
+                                    <p
+                                        style="position: absolute; left: 210px; bottom: 11px; color: black; font-size: 8px; font-weight: 900;">
+                                        09061648927</p>
+                                    <img src="../../assets/id_layouts/signature.png"
+                                        style="position: absolute; left: 65px; bottom: 20px; max-width:60px; max-height:30px; display: block; margin: auto;">
+                                </div>
                             <h3>Back</h3>
                         </div>
                     </div>
