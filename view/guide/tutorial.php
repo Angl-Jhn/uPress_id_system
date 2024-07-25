b62d825ef6e189a7667a78306c31d6092f4aef8b
Khalil-Est <qb202100149@wmsu.edu.ph>
2024-07-26 02:08:54 +0800

    TUTORIAL UPDATE

--- view/guide/tutorial.php
+++ view/guide/tutorial.php
@@ -2,7 +2,7 @@
 
     <div class="container">
         <h1 class="text-center" style="font-weight: 900;">STEP 1: CHOOSE THE RIGHT FORM</h1>
-        <p class="text-center" style="font-size: 20px">Are you a student or an employee?</P>
+        <p class="text-center" style="font-size: 20px">Are you a student or an employee? Select what applies.</P>
     </div>
     <div class="container-fluid col-md-6 d-flex align-items-center">
         <img src="../../assets/tutorial/step1.png" class="img-fluid mx-auto d-block" alt="STEP 1">
@@ -10,7 +10,7 @@
 
     <div class="container mt-5">
         <h1 class="text-center" style="font-weight: 900;">STEP 2: CHOOSE TRANSACTION TYPE</h1>
-        <p class="text-center" style="font-size: 20px">Are you a student or an employee?</P>
+        <p class="text-center" style="font-size: 20px">Select the proper transaction based on what you need.</P>
     </div>
     <div class="container-fluid col-md-6 d-flex align-items-center">
         <img src="../../assets/tutorial/step2.png" class="img-fluid mx-auto d-block" alt="STEP 1">
@@ -18,7 +18,7 @@
 
     <div class="container mt-5">
         <h1 class="text-center" style="font-weight: 900;">STEP 3: PROVIDE BASIC INFO</h1>
-        <p class="text-center" style="font-size: 20px">Choose the right form; are you a student or an employee?</P>
+        <p class="text-center" style="font-size: 20px">Make sure to provide proper information.</P>
     </div>
     <div class="container-fluid col-md-6 d-flex align-items-center">
         <img src="../../assets/tutorial/step3.png" class="img-fluid mx-auto d-block" alt="STEP 1">
@@ -26,7 +26,7 @@
 
     <div class="container mt-5">
         <h1 class="text-center" style="font-weight: 900;">STEP 4: PROVIDE EMERGENCY CONTACT INFO</h1>
-        <p class="text-center" style="font-size: 20px">Choose the right form; are you a student or an employee?</P>
+        <p class="text-center" style="font-size: 20px">Ensure that your emergency contact person is reachable even in times of crisis.</P>
     </div>
     <div class="container-fluid col-md-6 d-flex align-items-center">
         <img src="../../assets/tutorial/step4.png" class="img-fluid mx-auto d-block" alt="STEP 1">
@@ -34,7 +34,7 @@
 
     <div class="container mt-5">
         <h1 class="text-center" style="font-weight: 900;">STEP 5: PROVIDE ATTACHMENTS</h1>
-        <p class="text-center" style="font-size: 20px">Make sure that you provide clear scanned copies of each attachment.</P>
+        <p class="text-center" style="font-size: 20px">Make sure that the digital copies you provide are clear and are free of distrations.</P>
     </div>
     <div class="container-fluid col-md-6 d-flex align-items-center">
         <img src="../../assets/tutorial/step5.png" class="img-fluid mx-auto d-block" alt="STEP 1">
