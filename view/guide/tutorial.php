b624849f2f0608e3d8b68d834c8833445c953d92
Khalil-Est <qb202100149@wmsu.edu.ph>
2024-07-25 20:19:36 +0800

    TUTORIAL

--- view/guide/tutorial.php
+++ view/guide/tutorial.php
@@ -1,1 +1,51 @@
-tutorialshit
+<div class="container-fluid pt-5 pb-5">
+
+    <div class="container">
+        <h1 class="text-center" style="font-weight: 900;">STEP 1: CHOOSE THE RIGHT FORM</h1>
+        <p class="text-center" style="font-size: 20px">Are you a student or an employee?</P>
+    </div>
+    <div class="container-fluid col-md-6 d-flex align-items-center">
+        <img src="../../assets/tutorial/step1.png" class="img-fluid mx-auto d-block" alt="STEP 1">
+    </div>
+
+    <div class="container mt-5">
+        <h1 class="text-center" style="font-weight: 900;">STEP 2: CHOOSE TRANSACTION TYPE</h1>
+        <p class="text-center" style="font-size: 20px">Are you a student or an employee?</P>
+    </div>
+    <div class="container-fluid col-md-6 d-flex align-items-center">
+        <img src="../../assets/tutorial/step2.png" class="img-fluid mx-auto d-block" alt="STEP 1">
+    </div>
+
+    <div class="container mt-5">
+        <h1 class="text-center" style="font-weight: 900;">STEP 3: PROVIDE BASIC INFO</h1>
+        <p class="text-center" style="font-size: 20px">Choose the right form; are you a student or an employee?</P>
+    </div>
+    <div class="container-fluid col-md-6 d-flex align-items-center">
+        <img src="../../assets/tutorial/step3.png" class="img-fluid mx-auto d-block" alt="STEP 1">
+    </div>
+
+    <div class="container mt-5">
+        <h1 class="text-center" style="font-weight: 900;">STEP 4: PROVIDE EMERGENCY CONTACT INFO</h1>
+        <p class="text-center" style="font-size: 20px">Choose the right form; are you a student or an employee?</P>
+    </div>
+    <div class="container-fluid col-md-6 d-flex align-items-center">
+        <img src="../../assets/tutorial/step4.png" class="img-fluid mx-auto d-block" alt="STEP 1">
+    </div>
+
+    <div class="container mt-5">
+        <h1 class="text-center" style="font-weight: 900;">STEP 5: PROVIDE ATTACHMENTS</h1>
+        <p class="text-center" style="font-size: 20px">Make sure that you provide clear scanned copies of each attachment.</P>
+    </div>
+    <div class="container-fluid col-md-6 d-flex align-items-center">
+        <img src="../../assets/tutorial/step5.png" class="img-fluid mx-auto d-block" alt="STEP 1">
+    </div>
+
+    <div class="container mt-5">
+        <h1 class="text-center" style="font-weight: 900;">STEP 6: SUBMIT!</h1>
+        <p class="text-center" style="font-size: 20px">Press the submit button and wait for our email of confirmation. Thank you!</P>
+    </div>
+    <div class="container-fluid col-md-6 d-flex align-items-center">
+        <img src="../../assets/tutorial/step6.png" class="img-fluid mx-auto d-block" alt="STEP 1">
+    </div>
+
+</div>
